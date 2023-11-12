b8cdea33cb742a103b147f7afba1a16fb598f991
wdfwwkkee <denis.usmanov101@mail.ru>
2023-11-12 21:53:02 +0500

    regModalFULL

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,6 +13,18 @@ const signUpButton = document.getElementById("sign-btn");
 const modalReg = document.getElementById("regModal");
 const closeModalButton = document.getElementById("close-modal-btn");
 const html = document.getElementById("html");
+const tabButton = document.querySelectorAll(".tab_btn");
+const all_content = document.querySelectorAll(".tab-content");
+
+tabButton.forEach((tab, index)=> {
+  tab.addEventListener('click', (e)=> {
+    tabButton.forEach(tab=> {tab.classList.remove('active')});
+    tab.classList.add('active');
+    all_content.forEach(content=>{content.classList.remove('active')});
+    all_content[index].classList.add('active');
+  });
+})
+
 
 
 signUpButton.addEventListener("click", function() {
