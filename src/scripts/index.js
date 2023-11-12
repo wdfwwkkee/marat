1a7ea93da16f215d88625db92c88c2e855111ecf
wdfwwkkee <denis.usmanov101@mail.ru>
2023-11-12 20:46:41 +0500

    modalLogin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,24 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
  });
+
+
+
+
+const signUpButton = document.getElementById("sign-btn");
+const modalReg = document.getElementById("regModal");
+const closeModalButton = document.getElementById("close-modal-btn");
+const html = document.getElementById("html");
+
+
+signUpButton.addEventListener("click", function() {
+  modalReg.classList.add("open");
+  html.classList.add("lock");
+})
+
+closeModalButton.addEventListener("click", function() {
+  modalReg.classList.remove("open");
+  html.classList.remove("lock");
+})
+
+
